38d4fb675bb60f1f12cafb62690b9df49b22887d
DZakh <dzakh.dev@gmail.com>
2019-11-12 18:23:12 +0300

    DarkMode with localStorage

--- src/js/classes/dark-mode/index.js
+++ src/js/classes/dark-mode/index.js
@@ -1,17 +1,32 @@
 export default class DarkMode {
+  static isDark() {
+    return +localStorage.getItem('dark');
+  }
+
+  static storeMode() {
+    localStorage.setItem('dark', DarkMode.isDark() ? 0 : 1);
+  }
+
   static toggleIcon() {
     const darkmodeIcon = document.querySelector('.control__dark-mode-icon');
     darkmodeIcon.classList.toggle('icon-moon');
     darkmodeIcon.classList.toggle('icon-weather-01');
   }
 
-  static activate() {
-    const darkmodeBtn = document.getElementById('dark-mode');
+  static toggleMode() {
     const body = document.querySelector('body');
+    body.classList.toggle('darkmode');
+    DarkMode.toggleIcon();
+  }
 
+  static activate() {
+    if (DarkMode.isDark()) {
+      DarkMode.toggleMode();
+    }
+    const darkmodeBtn = document.getElementById('dark-mode');
     darkmodeBtn.addEventListener('click', () => {
-      DarkMode.toggleIcon();
-      body.classList.toggle('darkmode');
+      DarkMode.toggleMode();
+      DarkMode.storeMode();
     });
   }
 }
