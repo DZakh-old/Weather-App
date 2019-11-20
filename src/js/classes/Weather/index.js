9933c3203d7280c115393a4f34a63a2ac01428df
DZakh <dzakh.dev@gmail.com>
2019-11-20 15:28:42 +0300

    Cleaning up Weather class (it's not really a class now)

--- src/js/classes/Weather/index.js
+++ src/js/classes/Weather/index.js
@@ -1,65 +1,68 @@
-export default class Weather {
-  constructor(temp, id) {
-    this._main = temp.main;
-    [this._weather] = temp.weather;
-    this._wind = temp.wind;
-    this._date = new Date(temp.dt_txt);
-    this.clouds = temp.clouds.all;
-    this.id = id;
-  }
+const getDate = tempData => {
+  const formatDate = (date, options, lang) => new Intl.DateTimeFormat(lang, options).format(date);
 
-  get date() {
+  const getFormattedDate = options => {
     const lang = 'en-US';
-    const formatDate = options => new Intl.DateTimeFormat(lang, options).format(this._date);
+    const date = new Date(tempData.dt_txt);
+    return formatDate(date, options, lang);
+  };
 
-    return {
-      month: formatDate({ month: 'short' }),
-      weekday: formatDate({ weekday: 'short' }),
-      day: formatDate({ day: 'numeric' }),
-      time: formatDate({
-        hour: '2-digit',
-        minute: '2-digit',
-        hour12: false
-      }),
-      hour: formatDate({
-        hour: 'numeric',
-        hour12: false
-      })
-    };
-  }
+  return {
+    month: getFormattedDate({ month: 'short' }),
+    weekday: getFormattedDate({ weekday: 'short' }),
+    day: getFormattedDate({ day: 'numeric' }),
+    time: getFormattedDate({
+      hour: '2-digit',
+      minute: '2-digit',
+      hour12: false
+    }),
+    hour: getFormattedDate({
+      hour: 'numeric',
+      hour12: false
+    })
+  };
+};
 
-  get temp() {
-    return Math.round(this._main.temp);
-  }
+const getTemp = tempData => Math.round(tempData.main.temp);
 
-  get pressure() {
-    return this._main.pressure;
-  }
+const getPressure = tempData => tempData.main.pressure;
 
-  get humidity() {
-    return this._main.humidity;
-  }
+const getHumidity = tempData => tempData.main.humidity;
 
-  get weather() {
-    return {
-      code: this._weather.id,
-      status: this._weather.description,
-      iconId: this._weather.icon.slice(0, 2)
-    };
-  }
+const getWeather = tempData => {
+  const { id, description, icon } = tempData.weather[0];
+  return {
+    code: id,
+    status: description,
+    iconId: icon.slice(0, 2)
+  };
+};
 
-  get windSpeed() {
-    return Math.round(this._wind.speed);
-  }
+const getClouds = tempData => tempData.clouds.all;
 
-  get windDirection() {
-    const getWindDirectionId = deg => {
-      const numSides = 8;
-      const span = 360 / numSides;
+const getWindSpeed = tempData => Math.round(tempData.wind.speed);
 
-      return Math.ceil((deg + span / 2) / span) % numSides;
-    };
+const getWindDirection = tempData => {
+  const getWindDirectionId = deg => {
+    const numSides = 8;
+    const span = 360 / numSides;
 
-    return getWindDirectionId(this._wind.deg);
+    return Math.ceil((deg + span / 2) / span) % numSides;
+  };
+
+  return getWindDirectionId(tempData.wind.deg);
+};
+
+export default class Weather {
+  constructor(tempData, id) {
+    this.date = getDate(tempData);
+    this.temp = getTemp(tempData);
+    this.pressure = getPressure(tempData);
+    this.humidity = getHumidity(tempData);
+    this.weather = getWeather(tempData);
+    this.clouds = getClouds(tempData);
+    this.windSpeed = getWindSpeed(tempData);
+    this.windDirection = getWindDirection(tempData);
+    this.id = id;
   }
 }
