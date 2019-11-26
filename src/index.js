b348512c6bb231db65a9af013f60c989d10c8cdf
DZakh <dzakh.dev@gmail.com>
2019-11-26 18:03:56 +0300

    Small cleaning up

--- src/index.js
+++ src/index.js
@@ -2,9 +2,7 @@ import './stylesheets/main.scss';
 
 import '@babel/polyfill';
 
-import './js/helpers'; // It's empty
 import './js/functions';
-import './js/packedges';
 
 import Interface from './js/classes/Interface';
 
