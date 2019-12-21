9452be75e40946c4782a2af227587a4444de1b6b
DZakh <dzakh.dev@gmail.com>
2019-12-21 15:41:12 +0300

    Moving Interface class into function

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import './stylesheets/main.scss';
 import '@babel/polyfill';
 
 import { realVhHeight } from './js/functions/real-vh-height';
-import Interface from './js/handlers/Interface';
+import { activateInterface } from './js/handlers/interfaceHandler';
 
 realVhHeight();
-Interface.activate();
+activateInterface();
